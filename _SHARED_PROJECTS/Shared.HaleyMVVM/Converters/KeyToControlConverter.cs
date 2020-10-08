9780a9b0844b5b6823cb06f119178fcca2a0b27b
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2020-10-08 12:09:52 +0400

    Updated KeyToControlConverter

---
+++ _SHARED_PROJECTS/Shared.HaleyMVVM/Converters/KeyToControlConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace Haley.MVVM.Converters
+{
+    public class KeyToControlConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (!(value is string)) return null;
+                return ContainerStore.Singleton.controls.obtainControl((string)value);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+   
+}
