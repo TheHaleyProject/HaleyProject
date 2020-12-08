e85ba5d41b5a2cbed9444f7d0d85c078dfab4019
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2020-12-08 11:13:25 +0400

    Added string extensions.

---
+++ _CORE_PROJECTS/HaleyExtensions/Extensions/StringHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Haley.Utils
+{
+    public static class StringHelpers
+    {
+        public static string padCenter(this string source, int length, char character = '\u0000')
+        {
+            
+            int space_available = length - source.Length;
+            int pad_left = (space_available / 2) + source.Length; //Amount to pad left.
+            if (character == null)
+            {
+                return source.PadLeft(pad_left).PadRight(length);
+            }
+            else
+            {
+                return source.PadLeft(pad_left, character).PadRight(length, character);
+            }
+        }
+    }
+}
