d01a8b8fc0e0fb4a22fa8471fc435c2d7e507137
Senguttuvan Mahalingam <rmsmech@gmail.com>
2020-12-11 16:10:14 +0400

    Added helpers for string and bytes

--- _CORE_PROJECTS/HaleyExtensions/Extensions/StringHelpers.cs
+++ _CORE_PROJECTS/HaleyExtensions/Extensions/StringHelpers.cs
@@ -8,10 +8,9 @@ namespace Haley.Utils
     {
         public static string padCenter(this string source, int length, char character = '\u0000')
         {
-            
             int space_available = length - source.Length;
             int pad_left = (space_available / 2) + source.Length; //Amount to pad left.
-            if (character == null)
+            if (character == '\u0000')
             {
                 return source.PadLeft(pad_left).PadRight(length);
             }
@@ -19,6 +18,31 @@ namespace Haley.Utils
             {
                 return source.PadLeft(pad_left, character).PadRight(length, character);
             }
+        }
+        public static bool isBase64(this string input)
+        {
+            if (string.IsNullOrEmpty(input) || input.Length % 4 != 0
+               || input.Contains(" ") || input.Contains("\t") || input.Contains("\r") || input.Contains("\n"))
+                return false;
+            try
+            {
+                Convert.FromBase64String(input);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public static string toNumber(this string input)
+        {
+            string numbered_key = string.Empty;
+            foreach (var _char in input)
+            {
+                int index = (int)_char % 32;
+                numbered_key += index;
+            }
+            return numbered_key;
         }
     }
 }
