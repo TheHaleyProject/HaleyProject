1206b0ea2df4938782832e843cb88353d459c22a
Senguttuvan Mahalingam <rmsmech@gmail.com>
2020-12-13 19:08:01 +0400

    Modified Utils

---
+++ _CORE_PROJECTS/HaleyHelpers/Utils/IDUtils.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.IO;
+using System.Reflection;
+
+namespace Haley.Utils
+{
+    public static class IDUtils
+    {
+        public static string getMotherBoardID()
+        {
+            try
+            {
+                var mng_obj_searcher = new ManagementObjectSearcher("Select * From Win32_BaseBoard");
+                var collection = mng_obj_searcher.Get();
+                string id = null;
+                try
+                {
+                    foreach (ManagementObject mgt_obj in collection)
+                    {
+                        id = (string)mgt_obj["SerialNumber"];
+                    }
+                }
+                catch (Exception)
+                {
+                    return id;
+                }
+                return id;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static string getProcessorID()
+        {
+            try
+            {
+                var mng_obj_searcher = new ManagementObjectSearcher("Select * From Win32_processor");
+                var collection = mng_obj_searcher.Get();
+                string id = null;
+                try
+                {
+                    foreach (ManagementObject mgt_obj in collection)
+                    {
+                        id = (string)mgt_obj["ProcessorId"];
+                    }
+                }
+                catch (Exception)
+                {
+                    return id;
+                }
+                return id;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static string getID()
+        {
+            try
+            {
+                return (getMotherBoardID() + "###" +getProcessorID());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
