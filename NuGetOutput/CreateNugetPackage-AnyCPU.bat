8133c151e6054a8de3ad13d957e1a91b5619b2bd
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2020-12-27 18:07:41 +0400

    Changes to NuSpec

--- NuGetOutput/CreateNugetPackage-AnyCPU.bat
+++ NuGetOutput/CreateNugetPackage-AnyCPU.bat
@@ -7,7 +7,7 @@ EXIT)
 
 Echo Processing NugetPackage
 
-if exist ../_CORE_PROJECTS/HaleyMVVM/HaleyMVVM.csproj nuget pack ../_CORE_PROJECTS/HaleyMVVM/HaleyMVVM.csproj -Prop Configuration=Release -Symbols -SymbolPackageFormat snupkg
+if exist ../_CORE_PROJECTS/HaleyMVVM/HaleyMVVM.csproj nuget pack ../_CORE_PROJECTS/HaleyMVVM/HaleyMVVM.csproj -Prop Configuration=Release -IncludeReferencedProjects  -Symbols -SymbolPackageFormat snupkg
 
 Echo Process completed successfully. 
 PAUSE
