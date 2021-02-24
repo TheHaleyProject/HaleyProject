ce74c4c84cbee47a7b70f26be80ad85a34ac2ca5
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2021-02-24 16:32:24 +0400

    Haley WPF pilot start

--- _CORE_PROJECTS/HaleyWPF/Properties/Resources.Designer.cs
+++ _CORE_PROJECTS/HaleyWPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Haley.Wpf.Properties {
+namespace Haley.WPF.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Haley.Wpf.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Haley.Wpf.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Haley.WPF.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
