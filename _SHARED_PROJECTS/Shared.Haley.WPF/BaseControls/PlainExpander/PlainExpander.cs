b80a0253f0c50966987c288692bec1c55b644c22
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2021-02-28 04:12:36 +0400

    updated and finalized expander

--- _SHARED_PROJECTS/Shared.Haley.WPF/BaseControls/PlainExpander/PlainExpander.cs
+++ _SHARED_PROJECTS/Shared.Haley.WPF/BaseControls/PlainExpander/PlainExpander.cs
@@ -29,6 +29,16 @@ namespace Haley.WPF.BaseControls
             base.OnApplyTemplate();
         }
 
+        public bool ShowIcon
+        {
+            get { return (bool)GetValue(ShowIconProperty); }
+            set { SetValue(ShowIconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ShowIcon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ShowIconProperty =
+            DependencyProperty.Register(nameof(ShowIcon), typeof(bool), typeof(PlainExpander), new PropertyMetadata(true));
+
         public ImageSource Icon
         {
             get { return (ImageSource)GetValue(IconProperty); }
@@ -37,7 +47,7 @@ namespace Haley.WPF.BaseControls
 
         // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IconProperty =
-            DependencyProperty.Register(nameof(Icon), typeof(ImageSource), typeof(PlainExpander), new FrameworkPropertyMetadata(ResourceStore.getIcon(IconEnums.arrow_down_rounded.ToString())));
+            DependencyProperty.Register(nameof(Icon), typeof(ImageSource), typeof(PlainExpander), new FrameworkPropertyMetadata(null));
 
         public SolidColorBrush IconDefColor
         {
@@ -49,7 +59,25 @@ namespace Haley.WPF.BaseControls
         public static readonly DependencyProperty IconDefColorProperty =
             DependencyProperty.Register(nameof(IconDefColor), typeof(SolidColorBrush), typeof(PlainExpander), new FrameworkPropertyMetadata(null));
 
+        public ImageSource Arrow
+        {
+            get { return (ImageSource)GetValue(ArrowProperty); }
+            set { SetValue(ArrowProperty, value); }
+        }
 
+        // Using a DependencyProperty as the backing store for Arrow.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ArrowProperty =
+            DependencyProperty.Register(nameof(Arrow), typeof(ImageSource), typeof(PlainExpander), new FrameworkPropertyMetadata(ResourceStore.getIcon(IconEnums.arrow_down_rounded.ToString())));
+
+        public SolidColorBrush ArrowDefColor
+        {
+            get { return (SolidColorBrush)GetValue(ArrowDefColorProperty); }
+            set { SetValue(ArrowDefColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ArrowDefColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ArrowDefColorProperty =
+            DependencyProperty.Register(nameof(ArrowDefColor), typeof(SolidColorBrush), typeof(PlainExpander), new PropertyMetadata(null));
 
         public Brush ContentBackground
         {
