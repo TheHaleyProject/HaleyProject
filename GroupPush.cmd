ad6480a843dcb3ff6d007d9d6ee86c196119276f
Senguttuvan Mahalingam <rmsmech@gmail.com>
2023-10-21 20:25:48 +0400

    Group Pull and Push

---
+++ GroupPush.cmd
@@ -0,0 +1,5 @@
+@echo off
+echo pulling all folders
+rem for /D %%G in (..\*) do (echo %%G) && (cd %%G) && (git pull origin) && (cd ..)
+for /D %%G in (..\*) do ((echo %%G) && (cd %%G) &&  (cd) && (git push))
+pause
