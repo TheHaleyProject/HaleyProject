4f33c83985959746ee7d0224eb4419aa9f0213ba
Quetzal <rmsmech@outlook.com>
2025-02-15 10:39:54 +0400

    Groupclone command added

---
+++ GroupClone.cmd
@@ -0,0 +1,10 @@
+@echo off
+echo Move to previous folder
+cd..
+cd
+
+echo Cloning folders from Haley Project
+rem echo Haley | xargs -n1 | xargs -I{} git clone https://github.com/TheHaleyProject/Haley*
+
+for %%G in (HaleyHelpers.DB HaleyHelpers.Web HaleyHelpers HaleyRest HaleyExtensions HaleyLog HaleyWPF HaleyMVVM HaleyIOC HaleyWPFIconsPack HaleyVue HaleyAbstractions HaleyRuleEngine HaleyEvents HaleyXamlShared HaleyVSIX HaleyWPFFonts) do ((echo %%G) && (git clone https://github.com/TheHaleyProject/%%G))
+pause
